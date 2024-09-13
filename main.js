8481b5703c140d1828f4b153929bc3c45875db6b
jwstes <jwstes@gmail.com>
2024-09-13 11:41:31 +0800

    asd

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const serveIndex = require('serve-index');
+const path = require('path');
 const app = express()
 
+app.use('/public', serveIndex('files'));
+app.use('/public', express.static('files'));
+
 app.get('/', function (req, res) {
-  res.send('<h1 style="color:red;">Hey Lol</h1>')
+    res.sendFile(path.join(__dirname, 'html\\index.html'))
 })
 
 app.listen(3000)
